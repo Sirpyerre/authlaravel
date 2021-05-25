93c612c3bf31ef24bd4fbe2c17989d15623d8fd7
Pedro Rojas Reyes <pedro.rojas@gmail.com>
2021-05-25 14:55:08 -0500

    create migrations

---
+++ database/migrations/2021_05_25_194315_create_employees_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('first_name', 50);
+            $table->string('second_name', 50)->nullable();
+            $table->string('email', 100);
+            $table->date('bird_date')->nullable();
+            $table->string('phone')->nullable();
+            $table->unsignedBigInteger('position_id');
+            $table->integer('salary')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('position_id')->references('id')->on('positions');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
